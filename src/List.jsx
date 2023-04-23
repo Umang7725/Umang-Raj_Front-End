b1ceefa70dec46782762d0521960beced165d7c0
Umang Raj <100988384+Umang7725@users.noreply.github.com>
2023-04-23 09:07:09 +0530

    Update List.jsx

--- src/List.jsx
+++ src/List.jsx
@@ -10,7 +10,21 @@ const WrappedSingleListItem = ({
 }) => {
   return (
     <li
-      style={{ backgroundColor: isSelected ? 'green' : 'red'}}
+      style={{ backgroundColor: isSelected ? 'green' : 'red',
+      fontWeight: isSelected ? 'bold' : 'normal',
+      color: isSelected ? 'white' : 'black',
+      borderRadius: isSelected ? '10%' : '0',
+      listStyleType: 'none',
+      border: isSelected ? '2px solid black' : 'none',
+      transform: isSelected ? 'scale(1.1)' : 'none',
+      transition: 'transform 0.2s ease-in-out',
+      cursor: 'pointer' ,
+      letterSpacing: '0.8',
+      margin: '10px', 
+      height: '40px',
+      padding:'2px',
+      textAlign: 'center'
+    }}
       onClick={onClickHandler}
     >
       {text}
